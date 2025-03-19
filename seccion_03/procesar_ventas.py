87f3529f4de7e825199310272044735dc048fd18
Andres Felipe Monsalve Bocanegra <andresmonsalve@gestarinnovacion.local>
2025-03-18 23:06:56 -0500

    Sección 3: Agregamos script Python para generación de resumen de ventas

--- seccion_03/procesar_ventas.py
+++ seccion_03/procesar_ventas.py
@@ -8,6 +8,7 @@ df['total_ventas'] = df['cantidad'] * df['precio_unitario']
 resumen_ventas = df.groupby('producto')['total_ventas'].sum().reset_index()
 
 # Guardar el resumen en un nuevo archivo CSV
-resumen_ventas.to_csv('resumen_ventas.csv', index=False)
+resumen_ventas.to_csv("resumen_ventas.csv", index=False)
+
 
 print("Resumen de ventas guardado en 'resumen_ventas.csv'")
